77417a5f83f0d8e108e29b130914fafc9a8ffdf2
jimwell <jimwelllmanalo@gmail.com>
2025-03-12 23:59:25 +0800

    Create Rescue interface

---
+++ client/src/app/interfaces/rescue.ts
@@ -0,0 +1,11 @@
+// Define an interface representing the structure of rescue objects
+export interface Rescue {
+  name: string;
+  slug: string;
+  sex: string;
+  age: string;
+  size: string;
+  vetStatus: string;
+  description: string;
+  imgSrc: string;
+}

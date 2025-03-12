41760765b5aedbe9ff1a16fcac0b5c70b4152cbe
jimwell <jimwelllmanalo@gmail.com>
2025-03-12 23:58:15 +0800

    Setup Angular Routing

---
+++ client/src/app/app.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { RescuesListComponent } from './components/rescues-list/rescues-list.component';
+
+export const routes: Routes = [
+  {
+    path: 'rescues',
+    component: RescuesListComponent,
+  },
+];

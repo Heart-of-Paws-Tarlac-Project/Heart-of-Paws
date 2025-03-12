05effcc75a2e6ee77fa45a5d422f36877e212854
jimwell <jimwelllmanalo@gmail.com>
2025-03-13 00:00:18 +0800

    Setup Angular Project

---
+++ client/src/app/app.config.ts
@@ -0,0 +1,13 @@
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { provideHttpClient, withFetch } from '@angular/common/http'; //enables the usage of httpclient throughout the whole application
+
+import { routes } from './app.routes';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideHttpClient(withFetch()), //withFetch uses the fetch api which is much modern compared to what the default http client uses
+  ],
+};

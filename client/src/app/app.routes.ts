934c3f40936dff3826601a137275f82e37643c76
jimwell <jimwelllmanalo@gmail.com>
2025-03-13 17:27:50 +0800

    Implement Angular Routing

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { RescuesListComponent } from './components/rescues-list/rescues-list.component';
+import { RescueDetailComponent } from './components/rescue-detail/rescue-detail.component';
 
 export const routes: Routes = [
   {
     path: 'rescues',
     component: RescuesListComponent,
+  },
+  {
+    path: 'rescues/:slug',
+    component: RescueDetailComponent,
   },
 ];

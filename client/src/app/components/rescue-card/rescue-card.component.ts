78b679f5d9231426cb1ceb2a325382c61d6e7b22
jimwell <jimwelllmanalo@gmail.com>
2025-03-13 17:29:35 +0800

    Refactor usage of signals

--- client/src/app/components/rescue-card/rescue-card.component.ts
+++ client/src/app/components/rescue-card/rescue-card.component.ts
@@ -1,10 +1,12 @@
 import { Component, Input } from '@angular/core';
 import { Rescue } from '../../interfaces/rescue';
+import { RouterLink } from '@angular/router';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-rescue-card',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './rescue-card.component.html',
   styleUrl: './rescue-card.component.css',
 })

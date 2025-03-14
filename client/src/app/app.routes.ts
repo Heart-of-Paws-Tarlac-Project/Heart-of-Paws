9ba16fa57578388cc9d373325e2a60a9137228de
jimwell <jimwelllmanalo@gmail.com>
2025-03-15 01:15:28 +0800

    Add register route

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { RescuesListComponent } from './components/rescues-list/rescues-list.component';
 import { RescueDetailComponent } from './components/rescue-detail/rescue-detail.component';
+import { LoginFormComponent } from './components/login-form/login-form.component';
+import { RegisterFormComponent } from './components/register-form/register-form.component';
 
 export const routes: Routes = [
   {
@@ -10,5 +12,13 @@ export const routes: Routes = [
   {
     path: 'rescues/:slug',
     component: RescueDetailComponent,
+  },
+  {
+    path: 'register',
+    component: RegisterFormComponent,
+  },
+  {
+    path: 'login',
+    component: LoginFormComponent,
   },
 ];

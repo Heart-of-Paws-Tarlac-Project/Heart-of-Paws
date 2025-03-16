7323414afc632e01a1f1bf7e1be766017aa016a9
jimwell <jimwelllmanalo@gmail.com>
2025-03-16 20:46:34 +0800

    Add route guard for admin dashboard

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,10 +1,23 @@
 import { Routes } from '@angular/router';
-import { RescuesListComponent } from './components/rescues-list/rescues-list.component';
-import { RescueDetailComponent } from './components/rescue-detail/rescue-detail.component';
-import { LoginFormComponent } from './components/login-form/login-form.component';
-import { RegisterFormComponent } from './components/register-form/register-form.component';
+import { RescuesListComponent } from './components/pages/rescues-list/rescues-list.component';
+import { RescueDetailComponent } from './components/pages/rescue-detail/rescue-detail.component';
+import { LoginFormComponent } from './components/pages/login-form/login-form.component';
+import { RegisterFormComponent } from './components/pages/register-form/register-form.component';
+import { HomeComponent } from './components/pages/home/home.component';
+import { authGuard } from './guards/auth.guard';
+import { AdminDashboardComponent } from './components/pages/admin-dashboard/admin-dashboard.component';
+import { UnauthorizedComponent } from './components/pages/unauthorized/unauthorized.component';
 
 export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: '',
+  },
+  {
+    path: '',
+    component: HomeComponent,
+  },
   {
     path: 'rescues',
     component: RescuesListComponent,
@@ -20,5 +33,14 @@ export const routes: Routes = [
   {
     path: 'login',
     component: LoginFormComponent,
+  },
+  {
+    path: 'admin',
+    component: AdminDashboardComponent,
+    canActivate: [authGuard],
+  },
+  {
+    path: 'unauthorized',
+    component: UnauthorizedComponent,
   },
 ];

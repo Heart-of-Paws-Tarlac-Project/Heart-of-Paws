8a666b478dfac8f27a06e10f94ee667e3d70ddac
jangabb <jangabperez@gmail.com>
2025-03-16 21:24:40 +0800

    with-ui/ux

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,8 +1,15 @@
 import { Routes } from '@angular/router';
 import { RescuesListComponent } from './components/rescues-list/rescues-list.component';
 import { RescueDetailComponent } from './components/rescue-detail/rescue-detail.component';
+import { HomeComponent } from './components/pages/home/home.component';
+import { HelpComponent } from './components/pages/help/help.component';
+import { AboutComponent } from './components/pages/about/about.component';
 
 export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
   {
     path: 'rescues',
     component: RescuesListComponent,
@@ -10,5 +17,13 @@ export const routes: Routes = [
   {
     path: 'rescues/:slug',
     component: RescueDetailComponent,
+  },
+  {
+    path: 'howtohelp',
+    component: HelpComponent,
+  },
+  {
+    path: 'aboutus',
+    component: AboutComponent,
   },
 ];
---
+++ client/src/app/components/pages/home/home.component.ts
@@ -0,0 +1,16 @@
+import { Component, AfterViewInit } from '@angular/core';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css',
+})
+export class HomeComponent implements AfterViewInit {
+  ngAfterViewInit(): void {
+    AOS.init();
+  }
+}

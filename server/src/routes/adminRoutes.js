6f8fdcc3208cd2c95ad83b2f99dde8c86300682b
jimwell <jimwelllmanalo@gmail.com>
2025-03-19 06:39:35 +0800

    Add admin functionalities

---
+++ server/src/routes/adminRoutes.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const router = express.Router();
+const Applications = require("../models/application");
+const isAdmin = require("../middlewares/isAdmin");
+const adminController = require("../controllers/adminController");
+
+//approve or reject an application
+router.patch(
+  "/applications/:applicationId/status",
+  adminController.approveOrRejectApplication
+);
+
+
+
+module.exports = router;

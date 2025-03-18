5283dc9e4d6ee637f242f1d155905afa2b2ce37f
jimwell <jimwelllmanalo@gmail.com>
2025-03-19 02:21:30 +0800

    Add role check for user specifically

--- server/src/middlewares/isAuthenticated.js
+++ server/src/middlewares/isAuthenticated.js
@@ -1,5 +1,6 @@
 const isAuthenticated = (req, res, next) => {
-  if (req.session && req.session.user) {
+  if (req.session && req.session.user.role === "user") {
+    console.log(`req.session.user.role: ${req.session.user.role}`);
     return next(); //pass control to the next middleware
   }
   res.status(401).send({ message: "You must be logged in to access this" });

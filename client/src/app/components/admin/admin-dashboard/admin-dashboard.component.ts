d0dc71d0ca5310fd99a68b83a6f170c3453b9f30
jimwell <jimwelllmanalo@gmail.com>
2025-03-19 06:44:41 +0800

    Create admin dashboard

--- client/src/app/components/admin/admin-dashboard/admin-dashboard.component.ts
+++ client/src/app/components/admin/admin-dashboard/admin-dashboard.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RescuesListComponent } from '../../pages/rescues-list/rescues-list.component';
 
 @Component({
   selector: 'app-admin-dashboard',
   standalone: true,
-  imports: [],
+  imports: [RescuesListComponent],
   templateUrl: './admin-dashboard.component.html',
   styleUrl: './admin-dashboard.component.css'
 })

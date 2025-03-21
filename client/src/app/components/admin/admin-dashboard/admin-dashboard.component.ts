32788977671a117d3710ffd3302eebbd06b6c22a
jangabb <jangabperez@gmail.com>
2025-03-22 03:55:40 +0800

    added styles in admin dashboard

--- client/src/app/components/admin/admin-dashboard/admin-dashboard.component.ts
+++ client/src/app/components/admin/admin-dashboard/admin-dashboard.component.ts
@@ -1,19 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RescuesListComponent } from '../../pages/rescues-list/rescues-list.component';
-import { Router, RouterOutlet } from '@angular/router';
+import { Router, RouterLink, RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import { AdminNavbarComponent } from '../ui/admin-navbar/admin-navbar.component';
+import { AuthService } from '../../../services/auth.service';
+import { Subscription } from 'rxjs';
+import { AdminAuthService } from '../../../services/admin-auth.service';
 
 @Component({
   selector: 'app-admin-dashboard',
   standalone: true,
-  imports: [
-    RescuesListComponent,
-    RouterOutlet,
-    CommonModule,
-    AdminNavbarComponent,
-  ],
+  imports: [RescuesListComponent, RouterOutlet, CommonModule, RouterLink],
   templateUrl: './admin-dashboard.component.html',
   styleUrl: './admin-dashboard.component.css',
 })
-export class AdminDashboardComponent {}
+export class AdminDashboardComponent {
+  isAdminAuthenticated: boolean = false;
+  private adminAuthSub: Subscription | null = null;
+
+  constructor(
+    private adminAuthService: AdminAuthService,
+    private router: Router,
+    private authService: AuthService
+  ) {}
+
+  ngOnInit(): void {
+    this.adminAuthService.isAdminAuthenticated$.subscribe(
+      (isAdminAuthenticated) => {
+        console.log('is admin authenticated: ', isAdminAuthenticated);
+        this.isAdminAuthenticated = isAdminAuthenticated;
+      }
+    );
+  }
+
+  logoutAdmin() {
+    this.adminAuthService.logoutAdmin().subscribe({
+      next: (response) => {
+        console.log('Admin has been successfully logged out');
+        this.router.navigate(['/']);
+      },
+      error: (error) => {
+        console.error('Error logging out admin');
+      },
+    });
+  }
+
+  isSidebarOpen: boolean = false;
+
+  toggleSidebar() {
+    const sidebar = document.getElementById('sidebar');
+    if (sidebar) {
+      sidebar.classList.toggle('-translate-x-full');
+    }
+  }
+}
